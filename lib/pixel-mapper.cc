aaec54642e74c8838f7e4da594d7905faf04b8d3
David Roth <blaise.s.saunders@gmail.com>
2021-04-12 23:15:42 +1000

    test

--- lib/pixel-mapper.cc
+++ lib/pixel-mapper.cc
@@ -203,11 +203,11 @@ public:
 
 		// First 0 to 32 needs to go to y = 0 to y = 32
 		// Second 0 to 32 needs to go to y = 0 to y = 32 but offset by x width, also invert
-		if (y < 32)
+		if (y <= 32)
 		{
 			y = 32-y;
 		}
-		else if (y >= 32 && y < 64)
+		else if (y > 32 && y < 64)
 		{
 			// Get value of y from 0 to 32
 			y -= 32;
